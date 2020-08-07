5a1779692f9f24e0bce9ab7be34563c282308855
Yorwet <57901659+Yorwet@users.noreply.github.com>
2020-08-08 01:27:49 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -1,28 +1,28 @@
 n=int(input())
-a=int(n//3600)
-b=int(n%3600)//60
-c=int(n%3600)%60
-s=(a%100)//10
-d=(b%100)//10
-f=(c%100)//10
-if a%10==1 and s!=1:
-    print(a,"час", end=" ")
-if 1<a%10<5 and s!=1:
-    print(a,"часа", end=" ")
-if a%10>4 or s==1:
-    print(a,"часов", end=" ")
-if b!=0:
-    if b%10==1 and d!=1:
-        print(b,"минута", end=" ")
-    if 1<b%10<5 and d!=1:
-        print(b,"минуты", end=" ")
-    if b%10>4 or d==1:
-        print(b,"минут", end=" ")    
-if c!=0:
-    if c%10==1 and f!=1:
-        print(c,"секунда", end=" ")
-    if 1<c%10<5 and f!=1:
-        print(c,"секунды", end=" ")
-    if c%10>4 or f==1:
-        print(c,"секунд", end=" ")    
+hours=int(n//3600)
+minutes=int(n%3600)//60
+sec=int(n%3600)%60
+s=(hours%100)//10
+d=(minutes%100)//10
+f=(sec%100)//10
+if hours%10==1 and s!=1:
+    print(hours,"час", end=" ")
+if 1<hours%10<5 and s!=1:
+    print(hours,"часа", end=" ")
+if hours%10>4 or s==1:
+    print(hours,"часов", end=" ")
+if minutes!=0:
+    if minutes%10==1 and d!=1:
+        print(minutes,"минута", end=" ")
+    if 1<minutes%10<5 and d!=1:
+        print(minutes,"минуты", end=" ")
+    if minutes%10>4 or d==1:
+        print(minutes,"минут", end=" ")    
+if sec!=0:
+    if sec%10==1 and f!=1:
+        print(sec,"секунда", end=" ")
+    if 1<sec%10<5 and f!=1:
+        print(sec,"секунды", end=" ")
+    if sec%10>4 or f==1:
+        print(sec,"секунд", end=" ")    
 
