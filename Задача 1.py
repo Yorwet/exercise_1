e7f3686f0c0b5bd43b54b11b27b5aede96804a2f
Yorwet <57901659+Yorwet@users.noreply.github.com>
2020-08-07 23:21:26 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -6,23 +6,23 @@ s=(a%100)//10
 d=(b%100)//10
 f=(c%100)//10
 if a%10==1 and s!=1:
-    print(a,"час", end=" ")
+    print(a,"С‡Р°СЃ", end=" ")
 if 1<a%10<5 and s!=1:
-    print(a,"часа", end=" ")
+    print(a,"С‡Р°СЃР°", end=" ")
 if a%10>4 or s==1:
-    print(a,"часов", end=" ")
+    print(a,"С‡Р°СЃРѕРІ", end=" ")
 if b!=0:
     if b%10==1 and d!=1:
-        print(b,"минута", end=" ")
+        print(b,"РјРёРЅСѓС‚Р°", end=" ")
     if 1<b%10<5 and d!=1:
-        print(b,"минуты", end=" ")
+        print(b,"РјРёРЅСѓС‚С‹", end=" ")
     if b%10>4 or d==1:
-        print(b,"минут", end=" ")    
+        print(b,"РјРёРЅСѓС‚", end=" ")    
 if c!=0:
     if c%10==1 and f!=1:
-        print(c,"секунда", end=" ")
+        print(c,"СЃРµРєСѓРЅРґР°", end=" ")
     if 1<c%10<5 and f!=1:
-        print(c,"секунды", end=" ")
+        print(c,"СЃРµРєСѓРЅРґС‹", end=" ")
     if c%10>4 or f==1:
-        print(c,"секунд", end=" ")    
+        print(c,"СЃРµРєСѓРЅРґ", end=" ")    
 
